84b4c189a275e37bceb67acd4143a98a4f13d8da
SergioHenrique19 <garciasergiodm@gmail.com>
2018-12-07 16:51:33 -0200

    Codigo de pilha-encadeada generica finalizada

--- exemplos/pilha-encadeada.cpp
+++ exemplos/pilha-encadeada.cpp
@@ -32,14 +32,77 @@ class Pilha{
     public:
         Pilha();
         ~Pilha();
-        void empilharNoh();
-        void imprimirPilha();
+        int empilharNoh(int valor);
+        int desempilharNoh();
+        int espiaTopo();
+        void limparPilha();
+
 };
 
 //Construtor do noh
 Pilha::Pilha(){
     topo = NULL;
     tam = 0;
+}
+
+//Destrutor da pilha
+Pilha::~Pilha(){
+    limparPilha();
+}
+
+//Empilhar novo noh
+int Pilha::empilharNoh(int valor){
+    Noh* novo = new Noh(valor);
+
+    if(topo == NULL){
+        topo = novo;
+    }else{
+        novo->prox = topo;
+        topo = novo;
+    }
+
+    tam++;
+
+    return tam;
+}
+
+//Desempilhar topo da pilha
+int Pilha::desempilharNoh(){
+    if(topo != NULL){
+        Noh* aux = topo;
+        int result = aux->dado;
+
+        topo = topo->prox;
+        delete aux;
+        tam--;
+
+        return result;
+    }else{
+        return -1;
+    }
+}
+
+//Espia o topo da pilha
+int Pilha::espiaTopo(){
+    if(topo != NULL){
+        return topo->dado;
+    }else{
+        return -1;
+    }
+}
+
+//Limpar a pilha -- remover todo os elementos
+void Pilha::limparPilha(){
+    while(topo != NULL){
+        Noh* aux = topo;
+        int val = topo->dado;
+
+        topo = topo->prox;
+        delete aux;
+        tam--;
+
+        cout << val << " ";
+    }
 }
 
 //Funcao principal
@@ -52,7 +115,33 @@ int main(){
         cout << "\nOperacao: ";
         cin >> op;
 
-        switch(op)
+        switch(op){
+            case 'i':
+                cout << "Valor: ";
+                cin >> valor;
+                cout << "Tamanho: " << pilhanoh.empilharNoh(valor);
+                break;
+
+            case 'e':
+                cout << pilhanoh.espiaTopo();
+                break;
+
+            case 'd':
+                cout << "Resultado: " << pilhanoh.desempilharNoh();
+                break;
+            
+            case 'l':
+                pilhanoh.limparPilha();
+                break;
+            
+            case 'q':
+                cout << "Saindo...\n";
+                break;
+            
+            default:
+                cout << "\nComando invalido...\n";
+                break;
+        }
     }
     return 0;
 }
