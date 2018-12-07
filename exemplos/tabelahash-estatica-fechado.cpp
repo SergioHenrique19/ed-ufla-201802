706891778d7c3f534e8c981d2752f4c0f504ed22
SergioHenrique19 <garciasergiodm@gmail.com>
2018-12-07 17:41:43 -0200

    Adicionado codigo de tabela hash estatica com encadeamento aberto a finalizar

---
+++ exemplos/tabelahash-estatica-fechado.cpp
@@ -0,0 +1,14 @@
+/*
+ * Algoritmo de tabela hash estatica com encadeamento fechado
+ * 
+ * Copyright 2018 - Sérgio H. M. Garcia
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
